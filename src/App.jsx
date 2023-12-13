e5683e60502c27200a8137e86694382ebd00298d
jimdev90 <jimpv100190@gmail.com>
2023-12-13 18:18:23 -0500

    modificaciones del componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-function App({ name }) {
+import { API_URL} from "../env";
+
+function App() {
   return (
     <div>
-        <h1>Hola { name }</h1>
+        <h1>Hola { API_URL }</h1>
+        <p>Este es el sitio en desarrollo</p>
     </div>
   )
 }
